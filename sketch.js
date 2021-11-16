4b5a4943662423b8e125c9e2201088b79180e0ae
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-11-16 12:22:40 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
-var marks = [30,40,45,35];
+var notas = [10,5,7,6.5,4];
+
+function media_notas(){
+    var soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+    var media = soma / media.length;
+    console.log(media)
+}
 
 
 function setup() {
   createCanvas(400, 400);
-
+  
+  media_notas();
 }
 
 function draw() {
