deb13f9099f6b3defb85389ad2c670e177ea0e05
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-02-09 17:13:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var notas = [10,5,7,6.5,4];
 
 function media_notas(){
     var soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
-    var media = soma / media.length;
+    var media = soma / notas.length;
     console.log(media)
 }
 
